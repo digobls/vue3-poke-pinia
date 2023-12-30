063c143949148d0f35953281ea108b5b9d2aca16
digobls <digo.bls@hotmail.com>
2023-12-29 22:59:07 -0300

    feat: list pokemon

--- src/stores/pokemon.ts
+++ src/stores/pokemon.ts
@@ -1,52 +1,96 @@
-import { defineStore } from "pinia";
+import { defineStore } from 'pinia';
+import { computed, ref } from "vue";
 import axios from 'axios';
 
 interface Pokemon {
     id: number;
+    code: string;
     name: string;
+    description: string;
+    url: string;
+    image: string;
     types: string[];
+    stats: any[];
 }
 
-interface PokemonState {
-    pokemonList: Pokemon[];
-    isLoading: boolean;
-}
+export const usePokemonStore = defineStore("pokemon", () => {
+    // State
+    const listTypes = ref([]);
+    const listFullPokemon = ref([] as Pokemon[]);
+    const listPokemon = ref([] as Pokemon[]);
+    const dataPokemon = ref(null as Pokemon);
+    const isLoading = ref(false);
 
-export const usePokemonStore = defineStore({
-    id: "pokemon",
-    state: (): PokemonState => ({
-        pokemonList: [],
-        isLoading: false,
-    }),
-    actions: {
-        async fetchPokemonList() {
-            if (this.pokemonList.length === 0) {
-                try {
-                    this.isLoading = true;
+    // Actions
+    const fetchPokemonList = async () => {
+        isLoading.value = true;
+        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
+        const listResult = await Promise.all(response.data.results.map(async (pokemon: Pokemon) => {
+            // Get pokemon data
+            const pokemonDataResponse = await axios.get(pokemon.url);
+            const pokemonData = pokemonDataResponse.data;
 
-                    const response = await axios.get('https://pokeapi.co/api/v2/pokemon?limit=151');
-                    const data = response.data;
-
-                    const pokemonData = await Promise.all(data.results.map(async (pokemon: any) => {
-                        const detailResponse = await axios.get(pokemon.url);
-                        const detailData = detailResponse.data;
-
-                        return {
-                            id: detailData.id,
-                            name: detailData.name,
-                            types: detailData.types.map((type: any) => type.type.name),
-                        };
-                    }));
-
-                    console.log('pokemonData', pokemonData);
-                    this.pokemonList = pokemonData;
-                    this.isLoading = false;
-                } catch (error) {
-                    console.error('Error fetching Pokémon data:', error);
-                } finally {
-                    this.isLoading = false;
+            // Add types to array
+            pokemonData.types.forEach((type) => {
+                const capitalizedType = type.type.name.replace(/^\w/, (c) => c.toUpperCase());
+                if (!listTypes.value.includes(capitalizedType)) {
+                    listTypes.value = [...listTypes.value, capitalizedType];
                 }
-            }
-        },
-    },
+            });
+            listTypes.value.sort((a, b) => a.localeCompare(b));
+
+            // Get description of pokemon
+            const pokemonSpeciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonData.id}/`);
+            const pokemonSpeciesData = pokemonSpeciesResponse.data;
+            const description = pokemonSpeciesData.flavor_text_entries.find(entry => entry.language.name === 'en' && entry.version.name === 'ruby');
+
+            return {
+                id: pokemonData.id,
+                code: pokemonData.id < 10 ? `00${pokemonData.id}` : (pokemonData.id < 100 ? `0${pokemonData.id}` : `${pokemonData.id}`),
+                name: pokemonData.name,
+                url: pokemon.url,
+                image: pokemonData.sprites?.other?.['official-artwork']?.front_default,
+                description: description?.flavor_text,
+                types: pokemonData.types.map((type) => type.type.name),
+                stats: pokemonData.stats.map((stat) => ({
+                    name: stat.stat.name,
+                    baseValue: stat.base_stat,
+                }))
+            };
+        }));
+
+        listFullPokemon.value = listResult;
+        listPokemon.value = listResult;
+        isLoading.value = false;
+    }
+
+    const filterPokemonList = async (search: string, searchType: string) => {
+        if (search || searchType) {
+            listPokemon.value = listFullPokemon.value.filter((pokemon) => {
+                const hasName = pokemon.name.toLowerCase().includes(search.toLowerCase());
+                const hasType = !searchType || pokemon.types.includes(searchType.toLowerCase());
+                return hasName && hasType;
+            });
+        } else {
+            listPokemon.value = listFullPokemon.value;
+        }
+    };
+
+    const getDataPokemon = async (id: string) => {
+        dataPokemon.value = listFullPokemon.value.find(pokemon => pokemon.id === parseInt(id));
+    }
+
+    // Getters
+    const totalPokemon = computed(() => listPokemon.value.length);
+
+    return {
+        getDataPokemon,
+        fetchPokemonList,
+        filterPokemonList,
+        isLoading,
+        listTypes,
+        listPokemon,
+        totalPokemon,
+        dataPokemon
+    };
 });
--- src/views/ListView.vue
+++ src/views/ListView.vue
@@ -3,18 +3,18 @@
     <div class="block-list">
       <!-- Search -->
       <div v-if="!isLoading" class="pokemon-search">
-        <input v-model="search" type="text" placeholder="Search" class="pokemon-search__input" />
+        <input v-model="pokemonName" type="text" placeholder="Search" class="pokemon-search__input" />
 
-        <select v-model="searchType" class="pokemon-search__select">
+        <select v-model="pokemonType" class="pokemon-search__select">
           <option value="">Show All</option>
-          <option v-for="type in allTypes" :key="type" :value="type">
+          <option v-for="type in listTypes" :key="type" :value="type">
             {{ type }}
           </option>
         </select>
       </div>
 
       <!-- List -->
-      <a v-for="pokemon in filteredPokemonList" :key="pokemon.code" :class="[pokemon.types[0]]" :href="'/detail/' + pokemon.id" class="pokemon-card">
+      <a v-for="pokemon in listPokemon" :key="pokemon.code" :class="[pokemon.types[0]]" :href="'/detail/' + pokemon.id" class="pokemon-card">
         <img :src="pokemon.image" :alt="pokemon.name" class="pokemon-card__figure" />
         <span class="pokemon-card__code">{{ pokemon.code }}</span>
         <div class="pokemon-card__box">
@@ -31,15 +31,13 @@
 
       <!-- Loading -->
       <div v-if="isLoading" class="container-loading">
-        <a v-for="index in 20"
-           :key="index"
-           class="pokemon-card">
+        <a v-for="index in 151" :key="index" class="pokemon-card">
           <p class="pokemon-card--loading-adjust loading-on-element-custom"></p>
         </a>
       </div>
 
       <!-- Empty search -->
-      <div v-if="!filteredPokemonList.length && !isLoading" class="container-empty-search">
+      <div v-if="!listPokemon?.length && !isLoading" class="container-empty-search">
         <p class="container-empty-search__text">Pokémon not found.</p>
       </div>
     </div>
@@ -47,62 +45,44 @@
 </template>
 
 <script lang="ts">
-import { defineComponent } from "vue";
-import axios from "axios";
+import { defineComponent, ref, watch, onMounted } from "vue";
+import { usePokemonStore } from '@/stores/pokemon';
+import { storeToRefs } from "pinia";
 
 export default defineComponent({
   name: "ListPokemonComponent",
-  data() {
+  setup() {
+    const pokemonName = ref('');
+    const pokemonType = ref('');
+    const pokemonStore = usePokemonStore();
+    const { fetchPokemonList, filterPokemonList, totalPokemon } = pokemonStore;
+    const { listPokemon, listTypes, isLoading } = storeToRefs(pokemonStore);
+
+    onMounted(async () => {
+      await fetchPokemonList();
+    });
+
+    const searchPokemon = () => {
+      filterPokemonList(pokemonName.value, pokemonType.value);
+    };
+
+    watch([pokemonName, pokemonType], () => {
+      searchPokemon();
+    });
+
     return {
-      filteredPokemonList: [],
-      isLoading: false,
-      search: '',
-      searchType: '',
+      pokemonName,
+      pokemonType,
+      listTypes,
+      listPokemon,
+      totalPokemon,
+      isLoading
     };
   },
   methods: {
     getTypeIconUrl(type: string) {
       return `/src/assets/img/types/${type}.svg`;
-    },
-    async fetchPokemonData() {
-      try {
-        // Get pokemon from first generation
-        const response = await axios.get("https://pokeapi.co/api/v2/pokemon?limit=151");
-        const pokemonList = await Promise.all(response.data.results.map(async (pokemon) => {
-          // Get pokemon data
-          const pokemonDataResponse = await axios.get(pokemon.url);
-          const pokemonData = pokemonDataResponse.data;
-
-          // Get description of pokemon
-          const pokemonSpeciesResponse = await axios.get(`https://pokeapi.co/api/v2/pokemon-species/${pokemonData.id}/`);
-          const pokemonSpeciesData = pokemonSpeciesResponse.data;
-          const description = pokemonSpeciesData.flavor_text_entries.find(entry => entry.language.name === 'en' && entry.version.name === 'ruby');
-
-          return {
-            id: pokemonData.id,
-            code: pokemonData.id < 10 ? `00${pokemonData.id}` : (pokemonData.id < 100 ? `0${pokemonData.id}` : `${pokemonData.id}`),
-            name: pokemonData.name,
-            image: pokemonData.sprites?.other?.['official-artwork']?.front_default,
-            types: pokemonData.types.map((type) => type.type.name),
-            stats: pokemonData.stats.map((stat) => ({
-              name: stat.stat.name,
-              baseValue: stat.base_stat,
-            })),
-            description: description?.flavor_text
-          };
-        }));
-
-        console.log('pokemonList', pokemonList);
-        this.filteredPokemonList = pokemonList;
-      } catch (error) {
-        console.error("Erro ao buscar dados do Pokémon:", error);
-      } finally {
-        this.isLoading = false;
-      }
     }
-  },
-  mounted() {
-    this.fetchPokemonData();
   }
 });
 </script>
@@ -149,246 +129,247 @@ $color-map: (
 section {
   background-image: url('/src/assets/img/global/pokemon-bg.png');
   background-repeat: repeat;
-}
+  min-height: 100vh;
 
-.block-list {
-  padding: 83px 0;
-  position: relative;
-
-  .pokemon-search {
-    width: calc(100% - 40px);
-    display: block;
-    margin: 0 auto;
-
-    &__input,
-    &__select {
-      border-radius: 100px;
-      border: 1px solid $color-gray;
-      background-color: $color-white;
-      box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
-      display: block;
-      height: 30px;
-      font-family: $font-family-fira;
-      font-size: 10px;
-      color: $color-gray;
-      font-style: normal;
-      font-weight: 400;
-      line-height: normal;
-      padding: 0 10px;
-      background-repeat: no-repeat;
-      background-position: right 10px center;
-
-      &::before {
-        content: '';
-        position: absolute;
-        right: 10px;
-        top: 50%;
-        width: 12px;
-        height: 14px;
-        background-size: contain;
-      }
-    }
-
-    &__input {
-      width: calc(100% - 22px);
-      background-image: url('/src/assets/img/icons/search.svg');
-
-      &::placeholder {
-        color: $color-gray;
-      }
-    }
-
-    &__select {
-      width: 100%;
-      margin: 17px 0 30px;
-      appearance: none;
-      background-image: url('/src/assets/img/icons/arrow.svg');
-      -webkit-appearance: none;
-      -moz-appearance: none;
-
-      option {
-        font-family: $font-family-fira;
-        font-size: 10px;
-        color: $color-gray;
-      }
-    }
-  }
-
-  .pokemon-card {
-    width: calc(100% - 40px);
-    display: block;
+  .block-list {
+    padding: 83px 0;
     position: relative;
-    height: 65px;
-    padding: 0;
-    margin: 0 auto 15px;
-    border-radius: 8px;
-    background: $color-white;
-    border-width: 25px 0 10px 0;
-    border-style: solid;
-    text-decoration: none;
-    cursor: pointer;
-
-    &__code {
-      position: absolute;
-      top: -22px;
-      left: 9px;
-      font-family: $font-family-fira;
-      color: $color-white;
-      text-align: center;
-      font-size: 16px;
-      font-style: normal;
-      font-weight: 450;
-      line-height: normal;
-    }
-
-    &__figure {
-      position: absolute;
-      right: 0;
-      bottom: -9px;
-      width: 100px;
-      height: 100px;
-    }
-
-    &__box {
-      height: 65px;
-      width: 100%;
-      padding: 0 10px;
-      display: flex;
-      align-items: center;
-    }
-
-    &__type {
-      position: absolute;
-      display: flex;
-      align-items: center;
-      justify-content: center;
-      flex-direction: column;
-      top: -1px;
-      right: 111px;
-      margin: auto;
-      height: 65px;
-    }
-
-    &__image {
-      max-width: 18px;
-      margin-bottom: 9px;
-
-      &:last-of-type {
-        margin-bottom: 0;
-      }
-    }
-
-    &__name {
-      font-family: $font-family-fira;
-      font-size: 18px;
-      font-style: normal;
-      font-weight: 600;
-      line-height: 0;
-      margin: 0;
-      text-transform: capitalize;
-    }
-  }
-
-  @media only screen and (min-width: 581px) {
-    padding: 0;
-    display: flex;
-    flex-wrap: wrap;
-    max-width: 860px;
-    width: 100%;
-    margin: 0 auto;
 
     .pokemon-search {
-      width: 100%;
-      margin: 90px 0 70px;
-      text-align: right;
+      width: calc(100% - 40px);
+      display: block;
+      margin: 0 auto;
 
       &__input,
       &__select {
-        margin: 0;
-        width: 100%;
-        max-width: 200px;
-        display: inline-block;
+        border-radius: 100px;
+        border: 1px solid $color-gray;
+        background-color: $color-white;
+        box-shadow: 0 4px 4px 0 rgba(0, 0, 0, 0.25);
+        display: block;
+        height: 30px;
+        font-family: $font-family-fira;
+        font-size: 10px;
+        color: $color-gray;
+        font-style: normal;
+        font-weight: 400;
+        line-height: normal;
+        padding: 0 10px;
+        background-repeat: no-repeat;
+        background-position: right 10px center;
+
+        &::before {
+          content: '';
+          position: absolute;
+          right: 10px;
+          top: 50%;
+          width: 12px;
+          height: 14px;
+          background-size: contain;
+        }
       }
 
       &__input {
-        max-width: calc(200px - 22px);
-        margin-right: 21px;
+        width: calc(100% - 22px);
+        background-image: url('/src/assets/img/icons/search.svg');
+
+        &::placeholder {
+          color: $color-gray;
+        }
+      }
+
+      &__select {
+        width: 100%;
+        margin: 17px 0 30px;
+        appearance: none;
+        background-image: url('/src/assets/img/icons/arrow.svg');
+        -webkit-appearance: none;
+        -moz-appearance: none;
+
+        option {
+          font-family: $font-family-fira;
+          font-size: 10px;
+          color: $color-gray;
+        }
       }
     }
 
     .pokemon-card {
-      width: calc(25% - 20px);
-      max-width: 200px;
-      max-height: 156px;
-      margin: 10px 10px 68px;
+      width: calc(100% - 40px);
+      display: block;
+      position: relative;
+      height: 65px;
+      padding: 0;
+      margin: 0 auto 15px;
+      border-radius: 8px;
+      background: $color-white;
+      border-width: 25px 0 10px 0;
+      border-style: solid;
+      text-decoration: none;
+      cursor: pointer;
 
       &__code {
-        left: unset;
-        right: 5px;
+        position: absolute;
+        top: -22px;
+        left: 9px;
+        font-family: $font-family-fira;
+        color: $color-white;
+        text-align: center;
+        font-size: 16px;
+        font-style: normal;
+        font-weight: 450;
+        line-height: normal;
       }
 
       &__figure {
         position: absolute;
-        left: 0;
         right: 0;
-        bottom: 50px;
-        margin: 0 auto;
+        bottom: -9px;
+        width: 100px;
+        height: 100px;
       }
 
       &__box {
-        justify-content: center;
-        flex-direction: column;
+        height: 65px;
+        width: 100%;
+        padding: 0 10px;
+        display: flex;
+        align-items: center;
       }
 
       &__type {
-        top: 0;
-        right: unset;
-        left: 10px;
+        position: absolute;
+        display: flex;
+        align-items: center;
+        justify-content: center;
+        flex-direction: column;
+        top: -1px;
+        right: 111px;
+        margin: auto;
+        height: 65px;
+      }
+
+      &__image {
+        max-width: 18px;
+        margin-bottom: 9px;
+
+        &:last-of-type {
+          margin-bottom: 0;
+        }
       }
 
       &__name {
-        font-weight: 450;
+        font-family: $font-family-fira;
+        font-size: 18px;
+        font-style: normal;
+        font-weight: 600;
+        line-height: 0;
+        margin: 0;
+        text-transform: capitalize;
+      }
+    }
+
+    @media only screen and (min-width: 581px) {
+      padding: 0;
+      display: flex;
+      flex-wrap: wrap;
+      max-width: 860px;
+      width: 100%;
+      margin: 0 auto;
+
+      .pokemon-search {
+        width: 100%;
+        margin: 90px 0 70px;
+        text-align: right;
+
+        &__input,
+        &__select {
+          margin: 0;
+          width: 100%;
+          max-width: 200px;
+          display: inline-block;
+        }
+
+        &__input {
+          max-width: calc(200px - 22px);
+          margin-right: 21px;
+        }
+      }
+
+      .pokemon-card {
+        width: calc(25% - 20px);
+        max-width: 200px;
+        max-height: 156px;
+        margin: 10px 10px 68px;
+
+        &__code {
+          left: unset;
+          right: 5px;
+        }
+
+        &__figure {
+          position: absolute;
+          left: 0;
+          right: 0;
+          bottom: 50px;
+          margin: 0 auto;
+        }
+
+        &__box {
+          justify-content: center;
+          flex-direction: column;
+        }
+
+        &__type {
+          top: 0;
+          right: unset;
+          left: 10px;
+        }
+
+        &__name {
+          font-weight: 450;
+        }
       }
     }
   }
-}
 
-.container-loading {
-  width: 100%;
-  display: flex;
-  flex-wrap: wrap;
-
-  .pokemon-card {
-    border-color: #ccc;
-
-    &--loading-adjust {
-      height: 100%;
-      position: relative;
-      top: -20px;
-    }
-  }
-
-  @media only screen and (min-width: 581px) {
-    margin-top: 73px;
+  .container-loading {
+    width: 100%;
+    display: flex;
+    flex-wrap: wrap;
 
     .pokemon-card {
-      margin-bottom: 10px;
+      border-color: #ccc;
+
+      &--loading-adjust {
+        height: 100%;
+        position: relative;
+        top: -20px;
+      }
+    }
+
+    @media only screen and (min-width: 581px) {
+      margin-top: 73px;
+
+      .pokemon-card {
+        margin-bottom: 10px;
+      }
     }
   }
-}
 
-.container-empty-search {
-  display: block;
-  width: 100%;
+  .container-empty-search {
+    display: block;
+    width: 100%;
 
-  &__text {
-    font-family: $font-family-fira;
-    color: $color-white;
-    text-align: center;
-    font-size: 26px;
-    font-style: normal;
-    font-weight: 450;
-    line-height: normal;
+    &__text {
+      font-family: $font-family-fira;
+      color: $color-white;
+      text-align: center;
+      font-size: 26px;
+      font-style: normal;
+      font-weight: 450;
+      line-height: normal;
+    }
   }
 }
 </style>
